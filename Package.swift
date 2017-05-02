055ed327ce3854ca51bb408235e7a7c6a59d3fde
Aleksey Mashanov <a.mashanov@corp.mail.ru>
2017-05-02 22:41:35 +0300

    Add macOS compatibility

--- Package.swift
+++ Package.swift
@@ -1,25 +1,44 @@
 import PackageDescription
+
+#if os(Linux) || os(FreeBSD) || os(PS4) || os(Android) || CYGWIN
 import Glibc
+#elseif os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+import Darwin
+#endif
+
+let packageDir: String = {
+	let me = CommandLine.arguments[0]
+	var parts = me.characters.split(separator: "/", omittingEmptySubsequences: false).map(String.init)
+	parts[parts.endIndex - 1] = ""
+	return parts.joined(separator: "/")
+}()
 
 let package = Package(
-	name: "CPerl"
+	name: "CPerl",
+	pkgConfig: "../../.." + packageDir + "libperl"
 )
 
-func getenv(_ name: String) -> String? {
-	guard let value = Glibc.getenv(name) else { return nil }
+func env(_ name: String) -> String? {
+	guard let value = getenv(name) else { return nil }
 	return String(cString: value)
 }
 
-// Taken from swift-package-manager
-let tmpdir = getenv("TMPDIR") ?? getenv("TEMP") ?? getenv("TMP") ?? "/tmp/"
+let tmpdir = env("TMPDIR") ?? env("TEMP") ?? env("TMP") ?? "/tmp/"
 
-let me = CommandLine.arguments[0]
-if me[me.startIndex..<min(me.endIndex, tmpdir.endIndex)] != tmpdir {
-	var parts = me.characters.split(separator: "/", omittingEmptySubsequences: false).map(String.init)
-	parts[parts.endIndex - 1] = "generate-modulemap"
-	let command = parts.joined(separator: "/")
+if packageDir[packageDir.startIndex..<min(packageDir.endIndex, tmpdir.endIndex)] != tmpdir {
+	let command = packageDir + "prepare"
 
-	guard system(command) == 0 else {
-		fatalError("Failed to execute \(command)")
+	var pid = pid_t()
+	command.withCString {
+		guard posix_spawn(&pid, command, nil, nil, [UnsafeMutablePointer(mutating: $0), nil], nil) == 0 else {
+			fatalError("Failed to spawn \(command)")
+		}
+	}
+	var status: Int32 = 0
+	guard waitpid(pid, &status, 0) != -1 else {
+		fatalError("Failed to waitpid")
+	}
+	guard status == 0 else {
+		fatalError("\(command) terminated with status \(status)")
 	}
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CPerl
 
 ![Swift: 3.0](https://img.shields.io/badge/Swift-3.0-orange.svg)
-![OS: Linux](https://img.shields.io/badge/OS-Linux-brightgreen.svg)
+![OS: Linux | macOS](https://img.shields.io/badge/OS-Linux%20%7C%20macOS-brightgreen.svg)
 ![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
 
 Low-level Swift bindings for Perl.
